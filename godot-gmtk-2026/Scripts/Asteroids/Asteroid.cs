b7aff15236b20e3a62f973835b3dfd8892082224
VinciDa <marcusbthomsen@gmail.com>
2026-07-23 14:21:12 +0200

    Revert "Merge"
    This reverts commit be73b771b1b45715179c88f8d0483981f55b84dd, reversing
    changes made to c3494e3913cd16d77ae22fb07f7671b02a0eba7f.

--- godot-gmtk-2026/Scripts/Asteroids/Asteroid.cs
+++ godot-gmtk-2026/Scripts/Asteroids/Asteroid.cs
@@ -7,10 +7,7 @@ public partial class Asteroid : RigidBody3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var rand = new RandomNumberGenerator();
-
-		LinearVelocity = new Vector3(rand.RandfRange(-1, 1), 0, rand.RandfRange(-1, 1));
-		AngularVelocity = new Vector3(rand.RandfRange(-1, 1), rand.RandfRange(-1, 1), rand.RandfRange(-1, 1));
+		LinearVelocity = new Vector3(1, 0, 1);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
