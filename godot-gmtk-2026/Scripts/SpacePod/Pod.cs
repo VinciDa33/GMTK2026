e2eec1dfc1c2393dcf9af4464ec92110ff753df9
VinciDa <marcusbthomsen@gmail.com>
2026-07-23 15:13:32 +0200

    Space Pod

---
+++ godot-gmtk-2026/Scripts/SpacePod/Pod.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+using GodotGMTK2026.Scripts.Management;
+using GodotGMTK2026.Scripts.Player;
+
+namespace GodotGMTK2026.Scripts.SpacePod;
+
+public partial class Pod : Node
+{
+    public void PlayerEntered(Node3D body)
+    {
+        if (body is not PlayerController)
+            return;
+        GD.Print("Player entered the pod");
+        GameState.Instance.PlayerController.SetStopConsumption(true);
+    }
+
+    public void PlayerExited(Node3D body)
+    {
+        if (body is not PlayerController)
+            return;
+        GD.Print("Player left the pod");
+        GameState.Instance.PlayerController.SetStopConsumption(false);
+    }
+}
