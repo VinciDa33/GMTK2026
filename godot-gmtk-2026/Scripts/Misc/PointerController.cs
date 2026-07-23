7552865e8d7eef0aa1c1b3cd31b99ca175a90d37
VinciDa <marcusbthomsen@gmail.com>
2026-07-23 14:54:13 +0200

    Pointers

---
+++ godot-gmtk-2026/Scripts/Misc/PointerController.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+
+namespace GodotGMTK2026.Scripts.Misc;
+
+public partial class PointerController : Node3D
+{
+    [Export] private Node3D _origin;
+    [Export] private Node3D _target;
+    [Export] private float _orbitRadius;
+    public override void _Process(double delta)
+    {    
+        Vector3 direction = _target.GlobalPosition - _origin.GlobalPosition;        
+        direction.Y = 0;
+        direction = direction.Normalized();
+
+        GlobalPosition = _origin.GlobalPosition + direction * _orbitRadius;
+        
+        LookAt(GlobalPosition + direction, Vector3.Up);
+    }
+}
