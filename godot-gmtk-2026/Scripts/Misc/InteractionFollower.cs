ff6c08321f1baa43ceb869ab5131acb0ffbfd60d
VinciDa <marcusbthomsen@gmail.com>
2026-07-23 20:01:29 +0200

    Interaction Follower

---
+++ godot-gmtk-2026/Scripts/Misc/InteractionFollower.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+
+namespace GodotGMTK2026.Scripts.Misc;
+
+public partial class InteractionFollower : Node3D
+{
+    private Node3D _target;
+    [Export] public InteractionPrompt _prompt { get; private set; }
+    public void SetTarget(Node3D target)
+    {
+        _target = target;
+    }
+
+    public override void _Process(double delta)
+    {
+        if (_target == null)
+        {
+            QueueFree();
+            return;
+        }
+
+        GlobalPosition = _target.GlobalPosition + new Vector3(0f, 1f, 0f);
+    }
+}
